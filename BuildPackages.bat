eb3a8bb7fe782902505673455aa7569e69b1d102
Ingo <thewatcher@watchersnet.de>
2026-08-16 11:24:46 +0200

    updates

--- BuildPackages.bat
+++ BuildPackages.bat
@@ -1,6 +1,19 @@
-@SET FrameworkSDKDir=
-@SET PATH=%FrameworkDir%;%FrameworkSDKDir%;%PATH%
-@SET LANGDIR=EN
-@SET MSBUILDPATH="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MsBuild.exe"
+"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -nologo -latest -property installationPath > temp.txt
+set /p $MSBUILDROOT=<temp.txt
+del temp.txt
 
-%MSBUILDPATH% WatchersNET.TagCloud.sln /p:Configuration=Deploy /t:Clean;Build /p:WarningLevel=0 /flp1:logfile=errors.txt;errorsonly
+"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -property installationVersion > temp.txt
+Rem lower VS version
+Rem set /p $MSBUILDVER=<temp.txt
+Rem higher/unique VS
+for /f "delims==" %%a in (temp.txt) do set $MSBUILDVER=%%a
+del temp.txt
+
+for /f "tokens=1 delims=." %%G in ("%$MSBUILDVER%") do set Current=%%G.0
+Rem VS2017 => ~\MSBuild\15.0\Bin\MSBuild.exe
+Rem VS2019 => ~\MsBuild\Current\Bin\MSBuild.exe
+If "%Current%" NEQ "15.0" set Current=Current
+
+@set $MSBUILDPATH="%$MSBUILDROOT%\MsBuild\%Current%\Bin\MSBuild.exe"
+
+%$MSBUILDPATH% WatchersNET.TagCloud.slnx /p:Configuration=Deploy /t:Clean;Build /p:WarningLevel=0 /flp1:logfile=errors.txt;errorsonly
